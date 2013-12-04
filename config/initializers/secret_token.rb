0779b36464b38d1153a53a7dd0b179f611972f52
Ben Winter <ben.g.winter@gmail.com>
2013-12-04 11:45:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApolloApp::Application.config.secret_key_base = '9b6d105d5bf4484c3026d5b7f631561fc223f63900548c4b41b38fd00881e8a44afc7f19f5e314094a37b05a0b8e189672c7cbde1738cdf08749e2b283f9dfaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApolloApp::Application.config.session_store :cookie_store, key: '_apollo_app_session'
