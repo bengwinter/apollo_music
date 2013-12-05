5eb64b32c1f0b495217aa537d1cf9f90aec6c40c
Ben Winter <ben.g.winter@gmail.com>
2013-12-05 10:57:23 -0500

    changed minor things in migration

--- db/migrate/20131204230330_create_songs.rb
+++ db/migrate/20131204230330_create_songs.rb
@@ -1,11 +1,11 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.text :title, default: 'No Title'
-      t.text :artist, default: 'No Artist'
+      t.text :title, default: 'Untitled'
+      t.text :artist, default: 'Untitled'
       t.text :url
       t.text :source, default: 'SoundCloud'
-      t.text :album
+      t.text :album, default: 'Untitled'
       t.integer :plays
       t.integer :shares
       t.integer :favorites
